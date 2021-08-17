4c0ef6c81d53e90396afe28a4ff8a504623455af
Raymond <34949586+craychan@users.noreply.github.com>
2021-08-17 16:35:51 +0800

    Match statament { ... } use for read catfile

--- build_a_CLI/src/main.rs
+++ build_a_CLI/src/main.rs
@@ -1,11 +1,11 @@
-extern crate structopt;
 extern crate colored;
+extern crate structopt;
 
-use structopt::StructOpt;
 use colored::*;
+use structopt::StructOpt;
 
 #[derive(StructOpt)]
-struct Option {
+struct Options {
     #[structopt(default_value = "Meow!")]
     /// What does the cat say?
     message: String,
@@ -13,26 +13,38 @@ struct Option {
     #[structopt(short = "d", long = "dead")]
     /// Make the cat appear dead
     dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
 
-
 fn main() {
-    let options = Option::from_args();
+    let options = Options::from_args();
     let message = options.message;
 
-    let eyes = if options.dead {"x"} else{"o"};
+    let eyes = if options.dead { "x" } else { "o" };
+    //println!("{}", message);
+    match &options.catfile {
+        Some(path) => {
+            let cat_template =
+                std::fs::read_to_string(path).expect(&format!("could not read file{:?}", path));
+            let cat_picture = cat_template.replace("{eye}", eyes);
+            println!("{}", &cat_picture);
+        }
 
-    if message.to_lowercase() == "woof" {
-        eprint!("A cat shouldn't bark like a dog.")
+        None => {
+            if message.to_lowercase() == "woof" {
+                eprint!("A cat shouldn't bark like a dog.")
+            }
+            // let message = std::env::args().nth(1)
+            // .expect("missing the message. usage: catsay <message>");
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!("\\");
+            println!(" \\");
+            println!("    /\\_/\\");
+            println!("   ( {eye} {eye} )", eye = eyes.red().bold());
+            println!("   =( I )=");
+        }
     }
-
-    // let message = std::env::args().nth(1)
-        // .expect("missing the message. usage: catsay <message>");
-
-    println!("{}", message.bright_yellow().underline().on_purple());
-    println!("\\");
-    println!(" \\");
-    println!("    /\\_/\\");
-    println!("   ( {eye} {eye} )", eye=eyes.red().bold());
-    println!("   =( I )=");
 }
